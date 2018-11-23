7e91b76a3af7d8316fd30e765ceecc6e5be8276f
Cate Faull <cate.faull@payfast.co.za>
2018-11-23 16:38:19 +0200

    Improved messages on ad hoc button charges

--- includes/hooks/payfastviewinvoice.php
+++ includes/hooks/payfastviewinvoice.php
@@ -154,18 +154,12 @@ function oneClickPayment($params)
             {
                 header("Location:" . $params['systemurl'] . '/viewinvoice.php?id=' . $params['invoiceid']);
             }
-
-            if ( $invStatus != 'Paid' )
+            else
             {
                 ?><script type="text/javascript"> alert('The transaction has been successful. Currently your payment is being verified. Please contact us to confirm your payment.')</script><?php
                 logActivity( 'PayFast Ad Hoc payment with subscriptionid: ' . $guid . 'ITN failed to update the invoice to PAID in a reasonable time' );
                 $invFresh = $params['systemurl'].'/viewinvoice.php?id='.$params['invoiceid'];
                 header("refresh:5;url=$invFresh");
-            }
-            else
-            {
-                header("Location:" . $params['systemurl'] . '/viewinvoice.php?id=' . $params['invoiceid']);
-            }
         }
         else
         {
